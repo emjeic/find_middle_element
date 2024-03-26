a7c57123a7adc1af2d66fb76d2c4d17156f291b6
MARLIE JOHN CRUZ <marliejohnc.official@gmail.com>
2024-03-26 19:41:15 +0800

    Refactoring code for find the middle element

--- find_the_middle_element.rb
+++ find_the_middle_element.rb
@@ -1,25 +1,25 @@
-def find_middle_element(gimme)
-    if gimme[0] > gimme[1]
-        return gimme[0] < gimme[2] ? 0 : 2
+def find_middle_element(array)
+    first, second, third = array
+  
+    if (first > second && first < third) || (first < second && first > third)
+      return array.index(first)
+    elsif (second > first && second < third) || (second < first && second > third)
+      return array.index(second)
     else
-        return gimme[1] < gimme[2] ? 1 : 2
+      return array.index(third)
     end
-end
- 
-puts find_middle_element([2, 3, 1])
-puts find_middle_element([5, 10, 14])
+  end
+  
+  puts find_middle_element([2, 3, 1])
+  puts find_middle_element([5, 10, 14])
 
 # using ruby built-in method sort
 def find_middle_element(gimme)
-  # Sort the gimme in ascending order
   sorted_gimme = gimme.sort
   
-  # Find the middle element's index
   middle_index = sorted_gimme.sort[1]
   
   gimme.index(middle_index)
 end
   
-# Example usage:
-
 puts find_middle_element([2, 7, 5])
